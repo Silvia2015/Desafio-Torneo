3da3bf87abc9d9c3858efcbef956a423b2d03fb7
QukkiQ <oscar.zapata.g@mail.pucv.cl>
2022-11-07 22:28:31 -0300

    Actualizacion main

--- src/main/java/com/batalla/Personaje.java
+++ src/main/java/com/batalla/Personaje.java
@@ -1,5 +1,63 @@
 package com.batalla;
 
-public class Personaje {
+import org.apache.commons.math3.util.CombinatoricsUtils;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Personaje implements IActividades, IMovimientos {
+	
+	private String nombre;
+	private double poder;
+	private double energia;
+	
+	public Personaje() {
+		
+	}
+	public Personaje(String nombre, int poder, int energia) {
+		this.nombre = nombre;
+		this.poder = CombinatoricsUtils.factorial(poder);
+		this.energia = CombinatoricsUtils.factorial(energia);
+	}
+	
+	
+	public void ataqueBasico() {
+		System.out.println(this.nombre+", ha lanzado una onda de Ki.");
+	}
+	
+	public void ataqueAvanzado() {
+		System.out.println(this.nombre+", ha lanzado una Onda Vita Kaio-ken Dios x10.");
+	}
+	
+	public void defenderAtaque() {
+		System.out.println(this.nombre+", ha defendido con un Escudo de Energía.");
+	}
+	
+	public void esquivarAtaque() {
+		System.out.println(this.nombre+" ha esquivado con una Ilusión de Imagen");
+	}
+	
+	public void avanzar() {
+		System.out.println(this.nombre+", avanzó hacia adelante.");
+	}
+	
+	public void derecha() {
+		System.out.println(this.nombre+", giró a la derecha");
+	}
+	
+	public void izquierda() {
+		System.out.println(this.nombre+", giró a la izquierda");
+	}
+	
+	public void retroceder() {
+		System.out.println(this.nombre+", retrocedió.");
+	}
+	
+	public String toString() {
+		return "Hola, soy "+this.nombre+", mi poder alcanza "+this.poder+" y mi energía no supera los "+this.energia +".";
+	}
+	
 
 }
--- src/main/java/com/batalla/main.java
+++ src/main/java/com/batalla/main.java
@@ -3,8 +3,11 @@ package com.batalla;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Personaje goku = new Personaje("GOKÚ",5,10);
+		Personaje vegeta = new Personaje("VEGETA",6,9);
+		Torneo torneo = new Torneo(goku,vegeta);
+		torneo.presentarContrincantes();
+		torneo.pelea();
 	}
 
 }
